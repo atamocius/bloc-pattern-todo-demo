3ea60d274eae1c5c1f0f15676e75fce5c4e6865b
Daniel Anton Mata <daniel.mata@sykes.com>
2018-08-26 05:08:31 +0800

    Simplified retryableAsync implementation

--- web-client/src/utils/observableUtils.js
+++ web-client/src/utils/observableUtils.js
@@ -1,28 +1,47 @@
-import { defer } from 'rxjs';
-import { flatMap, scan, startWith, retryWhen, delay } from 'rxjs/operators';
+// import { defer } from 'rxjs';
+// import { flatMap, scan, startWith, retryWhen, delay } from 'rxjs/operators';
 
-export const retryDelay = (retries, timeDelay) =>
-  retryWhen(errors =>
-    errors.pipe(
-      delay(timeDelay),
-      startWith(1),
-      scan((count, currentErr) => {
-        console.warn('Retry #', count);
-        // console.log(`${count} >= ${retries}`, count >= retries);
-        if (count >= retries) {
-          // console.warn('NO MORE TRIES');
-          throw currentErr;
-        } else {
-          return (count += 1);
-        }
-      })
-    )
-  );
+// export const retryDelay = (retries, timeDelay) =>
+//   retryWhen(errors =>
+//     errors.pipe(
+//       delay(timeDelay),
+//       startWith(1),
+//       scan((count, currentErr) => {
+//         console.warn('Retry #', count);
+//         // console.log(`${count} >= ${retries}`, count >= retries);
+//         if (count >= retries) {
+//           // console.warn('NO MORE TRIES');
+//           throw currentErr;
+//         } else {
+//           return (count += 1);
+//         }
+//       })
+//     )
+//   );
 
-export const retryableAsync = (funcAsync, retries = 3, timeDelay = 500) =>
-  defer(funcAsync)
-    .pipe(
-      flatMap(async x => await x),
-      retryDelay(retries, timeDelay)
-    )
-    .toPromise();
+// export const retryableAsync = (funcAsync, retries = 3, timeDelay = 500) =>
+//   defer(funcAsync)
+//     .pipe(
+//       flatMap(async x => await x),
+//       retryDelay(retries, timeDelay)
+//     )
+//     .toPromise();
+
+export const retryableAsync = async (
+  asyncFunc,
+  retries = 3,
+  timeDelay = 500
+) => {
+  let retryCount = 0;
+
+  while (retryCount < retries) {
+    console.log(`Attempt #${retryCount}`);
+    try {
+      return await asyncFunc();
+    } catch (error) {
+      console.log(`Failed attempt #${retryCount}`);
+      await Promise.delay(timeDelay);
+      retryCount++;
+    }
+  }
+};
